f82c602000057151f44d801f954c876117fb8b5c
Saputra <saputra.uta50@gmail.com>
2024-06-11 00:05:43 +0800

    Basic

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { View, ScrollView, Image, Text, ImageBackground } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+      <ScrollView>
+        <Image source={logoImg} style={{ width: 200, height: 200 }} />
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          natus voluptates tempora voluptatum, quia sapiente!. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Possimus natus voluptates
+          tempora voluptatum, quia sapiente!. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Possimus natus voluptates tempora
+          voluptatum, quia sapiente!. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Possimus natus voluptates tempora voluptatum, quia
+          sapiente!. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Possimus natus voluptates tempora voluptatum, quia sapiente!. Lorem
+          ipsum dolor sit amet consectetur adipisicing elit. Possimus natus
+          voluptates tempora voluptatum, quia sapiente!.Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Possimus natus voluptates tempora
+          voluptatum, quia sapiente!. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Possimus natus voluptates tempora voluptatum, quia
+          sapiente!.
+        </Text>
+        <Image source={logoImg} style={{ width: 200, height: 200 }} />
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
