42c072cc9a43de8e9c163a21fc93bce78168643e
Saputra <saputra.uta50@gmail.com>
2024-06-11 01:33:40 +0800

    Stylesheet API

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import { View } from "react-native";
-import Greeting from "./components/Greeting";
+import { View, Text, StyleSheet } from "react-native";
+
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Greeting name="Saputra" />
-      <Greeting name="Uta" />
+    <View style={styles.container}>
+      <Text style={styles.title}>StyleSheet API</Text>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'plum',
+    padding: 60,
+  }, 
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+})
