fde95ea55ed00192c84afa75d14c879b2c217d8d
Saputra <saputra.uta50@gmail.com>
2024-06-11 01:20:48 +0800

    Custom Component

--- App.js
+++ App.js
@@ -1,23 +1,10 @@
-import { View, Button, Alert} from "react-native";
+import { View } from "react-native";
+import Greeting from "./components/Greeting";
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Button title="Alert" onPress={() => Alert.alert("Invalid data!")} />
-      <Button title="Alert" onPress={() => Alert.alert("Invalid data!", "Username is not valid")} />
-      <Button title="Alert" onPress={() => Alert.alert("Invalid data!", "Username is not valid", [
-        {
-          text: "Cancel",
-          onPress: () => {
-            console.log("Cancel Pressed");
-          }
-        },
-        {
-          text: "OK",
-          onPress: () => {
-            console.log("OK Pressed");
-          }
-        }
-      ])} />
+      <Greeting name="Saputra" />
+      <Greeting name="Uta" />
     </View>
   );
 }
